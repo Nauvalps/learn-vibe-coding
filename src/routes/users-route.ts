87a7156aed9d666a0e20f245c8d6fc218abe8826
Nauvalps <nauvalpurnomosidi@gmail.com>
2026-05-03 10:17:23 +0700

    feat: implement get current user endpoint with negative tests

--- src/routes/users-route.ts
+++ src/routes/users-route.ts
@@ -3,11 +3,18 @@ import { UsersController } from "../controllers/users-controller";
 
 const usersController = new UsersController();
 
-export const usersRoutes = new Elysia({ prefix: "/users" })
-  .post("/login", usersController.login, {
-    body: t.Object({
-      email: t.String(),
-      password: t.String(),
-    }),
-  })
-  .get("/verify", usersController.verify);
+export const usersRoutes = new Elysia()
+  .group("/users", (app) =>
+    app
+      .post("/login", usersController.login, {
+        body: t.Object({
+          email: t.String(),
+          password: t.String(),
+        }),
+      })
+      .get("/verify", usersController.verify)
+  )
+  .group("/api/users", (app) =>
+    app.get("/current", usersController.current)
+  );
+
